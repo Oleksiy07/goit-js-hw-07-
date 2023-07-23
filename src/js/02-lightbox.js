eee60ac6f72e9dc74f4cc00df71aa90fe4223898
Oleksiy Komarnitskyi <oleksij.komarnitskuj@gmail.com>
2023-07-23 20:39:31 +0300

    Виконав завдання 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
